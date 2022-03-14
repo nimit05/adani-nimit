49a758ed842a98809df79455fd31cbb077f56ee6
nimit05 <nimitwadhwa722@gmail.com>
2022-03-14 15:32:54 +0530

    question1 done

---
+++ question1.js
@@ -0,0 +1,36 @@
+let portfolio = [
+    {name: 'Mark', stock: 'FB'},
+    {name: 'Steve', stock: 'AAPL'},
+    {name: 'Tim', stock: 'AAPL'},
+    {name: 'Steve', stock: 'MSFT'},
+    {name: 'Bill', stock: 'MSFT'},
+    {name: 'Bill', stock: 'AAPL'},
+];
+    
+function helper(arr) {
+    let ans = [];
+    
+    arr.map((obj) => {
+        let temp = ans.find((ele) => {
+            return ele.stock && ele.stock === obj.stock
+        })
+
+        if (temp === undefined) {
+            let n = [];
+                n.push(obj.name);
+                ans.push({
+                    stock: obj.stock,
+                    name: n,
+                    count: 1
+                })
+        } else {
+            temp['name'].push(obj.name);
+            temp['count'] += 1;
+        }
+    })
+    
+    return ans;
+}
+
+let ab = helper(portfolio);
+console.log(ab);
